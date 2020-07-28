4e45dd27628048e08b1fd7b6cf5673b5958b7396
NasosG <53830262+NasosG@users.noreply.github.com>
2020-07-29 01:30:14 +0300

    typed effecct

--- js/scripts.js
+++ js/scripts.js
@@ -50,16 +50,19 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
   
-    // Floating label headings for the contact form
-    $(function() {
-      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-      }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-      }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-      });
-    });
+	// ========================================================================= //
+	//  Typed Js
+	// ========================================================================= //
+
+	var typed = $(".typed");
+
+	$(function() {
+		typed.typed({
+			strings: ["Thanasis Galatis"],
+			typeSpeed: /*100*/ Math.floor(Math.random() * 35) + 65 /*just to make the type speed a bit more random*/,
+		loop: false,
+		});
+	});
   
   })(jQuery); // End of use strict
   
